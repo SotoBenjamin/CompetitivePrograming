0881801a0a1787b4f1122ac8df39d483c6b3b611
Benjamin Soto <benjamin.soto@utec.edu.pe>
2025-03-27 09:42:58 -0500

    update

---
+++ training/MakeItAlternating.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cstdint>
+#include <vector>
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define DBG(x) cout << #x << " = " << x << '\n';
+#define DBGS(x) cout << #x << " = " << x << ", ";
+#define all(x) x.begin() , x.end()
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<pair<int,int>> vpii;
+typedef vector<ll> vl;
+typedef vector<pair<ll,ll>> vpll;
+     
+const ll mod = 1e9+7;
+const ll cc =  998244353;
+int nxt() {
+    int x; cin >> x;
+    return x;
+}  
+const int inf = INT_MAX;
+template<typename T>
+T minV(T a){
+    return a;
+}
+template<typename T, typename ... Args>
+T minV(T a , Args... arg){
+    T b = minVariadic(arg...);
+    return min(a,b);
+}
+int t,n;
+const int MAX= 2e5 + 1;
+ll fact[MAX];
+
+void factorial(){
+    fact[0] = 1;
+    for(ll i = 1 ; i < MAX ; i++){
+        fact[i] = ( fact[i-1] * i ) % cc;
+    }
+}
+
+
+void slv(){
+    string s; cin>>s;
+    int n = sz(s);
+    int cnt = 1;
+    vl v;
+    for(int i = 1 ; i < n ; i++){
+        if( s[i] == s[i-1]){
+            cnt++;
+        }
+        else{
+            v.push_back(cnt);
+            cnt = 1;
+        }
+    }
+
+    
+
+    v.push_back(cnt);
+    int len = n - sz(v);
+    ll ans = 1;
+    for(ll&i : v){
+        ans = (ans * i) % cc;
+    }
+    ans = (ans * fact[len]) %cc;
+    cout<<len<<" "<<ans<<"\n";
+}   
+     
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);    
+    freopen("output.txt","w",stdout);
+    #endif
+    t = 1;
+    cin>>t;
+    factorial();
+    while(t--) slv();
+    return 0;
+}
